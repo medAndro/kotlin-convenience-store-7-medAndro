b728a7b75819b17325f26a27df149a343ac8ee7e
medAndro <med4ndro@gmail.com>
2024-11-08 02:36:14 +0900

    fix: 증정품 추가 로직의 get과 buy가 반대로 설정되어 오동작하는 코드 수정

--- src/main/kotlin/store/domain/StoreService.kt
+++ src/main/kotlin/store/domain/StoreService.kt
@@ -9,9 +9,8 @@ class StoreService(
     private val productRepo: ProductRepository,
 ) {
 
-    fun writeReceipt(buyProductName: String, buyQuantity:Int) {
+    fun writeReceipt(buyProductName: String, buyQuantity: Int) {
         val product = productRepo.getProducts()[buyProductName]!!
-        //isOverflowPromoDiscount(product, buyProductName, buyQuantity)
         var getFree = 0
         val promotionName = product.getPromotionName()
         if (promotionName.isNullOrBlank()) {
@@ -23,16 +22,15 @@ class StoreService(
             val promoUnit = buy + get
             //추가여부 입력 기능
             val remain = buyQuantity % promoUnit
-            val boughtUnit = buyQuantity - remain
 
-            if (remain == get && ((buyQuantity + get) <= product.getPromoQuantity())) {
+            if (remain == buy && ((buyQuantity + get) <= product.getPromoQuantity())) {
                 val addPromoInfoMessage = Messages.INPUT_ADD_PROMOTION.ynMessage(buyProductName, get)
                 if(inputView.readValidYN(addPromoInfoMessage)){
                     getFree += get
                 }
             }
 
-            //재고부족 확인기능
+            // 일반적인 재고부족 확인기능
             if (product.getPromoQuantity() in 1..<buyQuantity) {
                 val canNotPromoAmount = buyQuantity - ((product.getPromoQuantity() / promoUnit) * promoUnit)
                 val infoMessage = Messages.INPUT_NOT_DISCOUNT.ynMessage(buyProductName, canNotPromoAmount)
@@ -43,21 +41,10 @@ class StoreService(
                     val unitCount = (product.getPromoQuantity() / promoUnit)
                     productRepo.addReceipt(product, promoUnit * unitCount, get * unitCount)
                 }
-
             }
+
             val unitCount = ((buyQuantity + getFree) / promoUnit)
             productRepo.addReceipt(product, buyQuantity + getFree, get * unitCount)
         }
-//        if (product.getPromoQuantity() > quantity) {
-//            checkPromotion(
-//                getPromotions()[product.getPromotion()],
-//                quantity = quantity,
-//            )
-//        }.
-
     }
-
-//    private fun checkPromotion(promotion: Promotion?, quantity:Int): Promotion {
-//        // 현재 dd 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)
-//    }
 }
